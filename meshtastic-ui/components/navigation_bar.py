ecaeaa01394f4fec583460dcb9a4fe5188fef8f8
milovanderlinden <milo@dogodigi.net>
2024-06-07 09:54:14 +0200

    Using navigation, NavigationBar working

--- meshtastic-ui/components/navigation_bar.py
+++ meshtastic-ui/components/navigation_bar.py
@@ -1,9 +1,23 @@
 import flet as ft
+from flet_core import ControlEvent
+from flet_navigator import PageData
 
 
 class NavigationBar(ft.NavigationBar):
-    def __init__(self, selected_index: int = 0):
+
+    def clicked(self, e: ControlEvent):
+        if e == "default":
+            nav_dest = 0
+        else:
+            nav_dest = e.control.selected_index
+        if nav_dest == 1:
+            self.pg.navigator.navigate('nodes', self.pg.page)
+        elif nav_dest == 3:
+            self.pg.navigator.navigate('devices', self.pg.page)
+
+    def __init__(self, pg: PageData, selected_index: int = 0,):
         super().__init__()
+        self.pg = pg
         self.selected_index = selected_index
         self.destinations = [
             ft.NavigationDestination(icon=ft.icons.CHAT),
@@ -12,3 +26,4 @@ class NavigationBar(ft.NavigationBar):
             ft.NavigationDestination(icon=ft.icons.WIFI_TETHERING),
             ft.NavigationDestination(icon=ft.icons.SETTINGS_APPLICATIONS_OUTLINED),
         ]
+        self.on_change = self.clicked
--- meshtastic-ui/main.py
+++ meshtastic-ui/main.py
@@ -1,81 +1,74 @@
 import flet as ft
+from flet_navigator import VirtualFletNavigator, ROUTE_404, NavigatorAnimation
+from flet_navigator import PageData, route
 from components import AppBar, NavigationBar, NodeGridComponent
 
 
+@route('/')
+def main_page(pg: PageData) -> None:
+    pg.page.vertical_alignment = ft.MainAxisAlignment.CENTER,
+    pg.page.horizontal_alignment = ft.CrossAxisAlignment.CENTER,
+    pg.page.spacing = 26
+    pg.page.add(
+        AppBar("Home"),
+        ft.Text(value="Home", size=30),
+        ft.ElevatedButton(text='Show nodes', on_click=lambda _: pg.navigator.navigate('nodes', pg.page)),
+        ft.ElevatedButton(text='Show devices', on_click=lambda _: pg.navigator.navigate('devices', pg.page)),
+        NavigationBar(pg, 0)
+    )
+
+
+@route('nodes')
+def nodes_page(pg: PageData) -> None:
+    pg.page.vertical_alignment = ft.MainAxisAlignment.CENTER,
+    pg.page.horizontal_alignment = ft.CrossAxisAlignment.CENTER,
+    pg.page.spacing = 26
+    pg.page.add(
+        AppBar("Nodes"),
+        ft.Text(value="Nodes", size=30),
+        ft.ElevatedButton(text='Back', on_click=lambda _: pg.navigator.navigate('/', pg.page)),
+        NavigationBar(pg, 1),
+        NodeGridComponent()
+    )
+
+
+@route('devices')
+def devices_page(pg: PageData) -> None:
+    pg.page.vertical_alignment = ft.MainAxisAlignment.CENTER,
+    pg.page.horizontal_alignment = ft.CrossAxisAlignment.CENTER,
+    pg.page.spacing = 26
+    cg = ft.RadioGroup(content=ft.Column([
+        ft.Radio(value="red", label="None"),
+        ft.Radio(value="green", label="Meshtastic_9e14")]), value="red")
+    pg.page.add(
+        AppBar("Devices"),
+        ft.Text(value="Devices", size=30),
+        ft.ElevatedButton(text='Back', on_click=lambda _: pg.navigator.navigate('/', pg.page)),
+        NavigationBar(pg, 3),
+        ft.Text("Not connected, select radio below"),
+        cg
+    )
+
+
+def route_404(pg: PageData) -> None:
+    pg.page.add(ft.Text(f'404'))
+
+
 def main(page: ft.Page) -> None:
-    page.title = "Meshtastic - python"
+    # Initialize navigator.
+    flet_navigator = VirtualFletNavigator(
+        routes={
+            '/': main_page,
+            'nodes': nodes_page,
+            'devices': devices_page,
+            ROUTE_404: route_404  # 404 page route.
+        },
+        # route_changed_handler=lambda _route: print(f'Route changed!: {_route}'),
+        navigator_animation=NavigatorAnimation(NavigatorAnimation.NONE)
+    )
 
-    def route_change(e: ft.RouteChangeEvent) -> None:
-        page.views.clear()
-
-        page.views.append(
-            ft.View(
-                route="/",
-                controls=[
-                    AppBar("Home"),
-                    ft.Text(value="Home", size=30),
-                    ft.ElevatedButton(text='Show nodes', on_click=lambda _: page.go('/nodes')),
-                    ft.ElevatedButton(text='Show devices', on_click=lambda _: page.go('/devices')),
-                    NavigationBar(0)
-                ],
-                vertical_alignment=ft.MainAxisAlignment.CENTER,
-                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                spacing=26
-            )
-        )
-
-        if page.route == '/nodes':
-            page.views.append(
-                ft.View(
-                    route="/nodes",
-                    controls=[
-                        AppBar("Nodes"),
-                        ft.Text(value="Nodes", size=30),
-                        ft.ElevatedButton(text='Back', on_click=lambda _: page.go('/')),
-                        NavigationBar(4),
-                        NodeGridComponent()
-                    ],
-                    vertical_alignment=ft.MainAxisAlignment.CENTER,
-                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                    spacing=26
-                )
-            )
-
-        if page.route == '/devices':
-            cg = ft.RadioGroup(content=ft.Column([
-                ft.Radio(value="red", label="None"),
-                ft.Radio(value="green", label="Meshtastic_9e14")]), value="red")
-
-            page.views.append(
-                ft.View(
-                    route="/devices",
-                    controls=[
-                        AppBar("Devices"),
-                        ft.Text(value="Devices", size=30),
-                        ft.ElevatedButton(text='Back', on_click=lambda _: page.go('/')),
-                        NavigationBar(3),
-                        ft.Text("Not connected, select radio below"),
-                        cg
-                    ],
-                    vertical_alignment=ft.MainAxisAlignment.CENTER,
-                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                    spacing=26
-                )
-            )
-
-        page.update()
-
-    def view_pop(e: ft.ViewPopEvent) -> None:
-        page.views.pop()
-        top_view: ft.View = page.views[-1]
-        page.go(top_view.route)
-
-    page.on_route_change = route_change
-    page.on_view_pop = view_pop
-    page.go(page.route)
+    flet_navigator.render(page)  # Render current page.
 
 
 if __name__ == '__main__':
     ft.app(target=main)
-
-
